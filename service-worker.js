bc83ebdf08e249a0b312db56912a9e0e67deb8ee
wtu-sos <595388976@qq.com>
2019-02-19 23:28:23 +0800

    update

---
+++ service-worker.js
@@ -0,0 +1,76 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "25637745606e560b0db383d34d363f51"
+  },
+  {
+    "url": "assets/css/0.styles.68be1f38.css",
+    "revision": "05509c66b5feb298767fcea9557bf1cf"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/2.0f4038f2.js",
+    "revision": "ac3c07dd1687b332f8d66d63c115fd6c"
+  },
+  {
+    "url": "assets/js/3.9245d31f.js",
+    "revision": "ad07cffe71950f62e9c95bf8d55f9703"
+  },
+  {
+    "url": "assets/js/4.5ba35f69.js",
+    "revision": "fea0453b59ad31d4edaf6ac124d9f113"
+  },
+  {
+    "url": "assets/js/app.8831b87a.js",
+    "revision": "511e9221a9a70a1ea57dc10216a47348"
+  },
+  {
+    "url": "img/index.jpeg",
+    "revision": "9855a9d336d5cf1129fee3daa2ac137d"
+  },
+  {
+    "url": "index.html",
+    "revision": "6b5502c9dd874405bf3c60fca183c200"
+  },
+  {
+    "url": "logo.png",
+    "revision": "4270831bef759edbe71b1a481731d893"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
